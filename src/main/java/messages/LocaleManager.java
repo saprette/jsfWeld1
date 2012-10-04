22efbc4cf1b33f9c82742bdfa1d79f9e18abcb72
talaran <samuel@prette.fr>
2012-10-04 14:00:56 +0200

    Added support for french and english language. Edited the header part of the template to allow the user to change the language of the running application. Added a backing bean LocaleManager in order to manage the logic of the current locale for each users.

---
+++ src/main/java/messages/LocaleManager.java
@@ -0,0 +1,66 @@
+package messages;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ValueChangeEvent;
+import javax.inject.Named;
+import java.io.Serializable;
+import java.util.Locale;
+import java.util.Map;
+import java.util.ResourceBundle;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Named
+@SessionScoped
+public class LocaleManager implements Serializable {
+
+    private String localeCode;
+    private ResourceBundle bundle;
+    private Map<String, Object> countries = new ConcurrentHashMap<String, Object>();
+
+    @PostConstruct
+    public void init() {
+        refreshCountries();
+    }
+
+    public void refreshCountries() {
+        countries.clear();
+        countries.put(getBundle().getString("english"), Locale.ENGLISH);
+        countries.put(getBundle().getString("french"), Locale.FRENCH);
+    }
+
+    public Map<String, Object> getCountries() {
+        return countries;
+    }
+
+    public ResourceBundle getBundle() {
+        if (bundle == null) {
+            FacesContext context = FacesContext.getCurrentInstance();
+            bundle = context.getApplication().getResourceBundle(context, "msgs");
+        }
+        return bundle;
+    }
+
+    public String getLocaleCode() {
+        return localeCode;
+    }
+
+
+    public void setLocaleCode(String localeCode) {
+        this.localeCode = localeCode;
+    }
+
+    public void countryLocaleCodeChanged(ValueChangeEvent e) {
+        String newLocaleValue = e.getNewValue().toString();
+        for (Map.Entry<String, Object> entry : countries.entrySet()) {
+            if (entry.getValue().toString().equals(newLocaleValue)) {
+                FacesContext context = FacesContext.getCurrentInstance();
+                context.getViewRoot().setLocale((Locale) entry.getValue());
+                bundle = context.getApplication().getResourceBundle(context, "msgs");
+                refreshCountries();
+                return;
+            }
+        }
+    }
+}
